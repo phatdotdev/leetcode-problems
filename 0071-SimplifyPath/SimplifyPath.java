90570dc813d15dcc5e881b6e1a0fa68963944902
Phat13092004 <dtphat.130904@gmail.com>
2025-09-20 13:17:21 +0700

    SimplifiPath

---
+++ 0071-SimplifyPath/SimplifyPath.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+
+}
+
+/*
+ * public String simplifyPath(String path) {
+ * Deque<String> stack = new ArrayDeque<>();
+ * String[] parts = path.split("/");
+ * 
+ * for (String part : parts) {
+ * if (part.equals("") || part.equals(".")) {
+ * continue;
+ * } else if (part.equals("..")) {
+ * if (!stack.isEmpty()) {
+ * stack.pollLast();
+ * }
+ * } else {
+ * stack.addLast(part);
+ * }
+ * }
+ * 
+ * return "/" + String.join("/", stack);
+ * }
+ */
