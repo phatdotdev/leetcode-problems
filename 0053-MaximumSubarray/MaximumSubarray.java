84a81b4c9564e15e2904f61d485731f7fa43ddbe
Phat13092004 <dtphat.130904@gmail.com>
2025-09-21 10:04:35 +0700

    Maximum SubArray

--- 0053-MaximumSubarray/MaximumSubarray.java
+++ 0053-MaximumSubarray/MaximumSubarray.java
@@ -1,12 +1,25 @@
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int n = nums.length, ans = nums[0];
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            ans = Math.max(dp[i], ans);
+        int sum = 0;
+        int max = 0;
+        for (int num : nums) {
+            if (sum < 0)
+                sum = 0;
+            sum += num;
+            max = max > sum ? max : sum;
         }
-        return ans;
+        return max;
     }
 }
+/*
+ * public int maxSubArray(int[] nums) {
+ * int n = nums.length, ans = nums[0];
+ * int[] dp = new int[n];
+ * dp[0] = nums[0];
+ * for (int i = 1; i < n; i++) {
+ * dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+ * ans = Math.max(dp[i], ans);
+ * }
+ * return ans;
+ * }
+ */
