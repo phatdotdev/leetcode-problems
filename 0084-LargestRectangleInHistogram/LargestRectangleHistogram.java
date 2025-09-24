a44e00513a3ddbe0c2d4cc38a101598c2a7b9dfa
Phat13092004 <dtphat.130904@gmail.com>
2025-09-24 21:54:55 +0700

    LargestRectangle

--- 0084-LargestRectangleInHistogram/LargestRectangleHistogram.java
+++ 0084-LargestRectangleInHistogram/LargestRectangleHistogram.java
@@ -2,7 +2,22 @@
 import java.util.Stack;
 
 public class LargestRectangleHistogram {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            int curr = (i == n ? 0 : heights[i]);
+            while (!stack.isEmpty() && curr < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : (i - stack.peek() - 1);
+                maxArea = Math.max(height * width, maxArea);
+            }
+            stack.push(i);
+        }
 
+        return maxArea;
+    }
 }
 
 /*
