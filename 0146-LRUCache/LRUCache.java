bd102966c4d4a10a1e4b64483e61efe517ac5dc8
Phat13092004 <dtphat.130904@gmail.com>
2025-10-04 10:35:21 +0700

    LRUCache

---
+++ 0146-LRUCache/LRUCache.java
@@ -0,0 +1,62 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+    public class Node {
+        public int key, value;
+        public Node prev, next;
+
+        public Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        Node node = new Node(key, value);
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insertToHead(node);
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToHead(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.prev = head;
+        node.next.prev = node;
+        head.next = node;
+    }
+}
