e5e8d5037462733a1d9d4c35251daf8695fc26f5
Phat13092004 <dtphat.130904@gmail.com>
2025-10-14 23:08:35 +0700

    SQRT

---
+++ 0069-SQRT/SQRT.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if x == mid * mid:
+                return mid
+            elif x > mid * mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
